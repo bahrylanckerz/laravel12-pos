d8c1359911aa814ec1d4ff57c5957a008f656602
Bahry Lanckerz <bahrylanckerz@gmail.com>
2025-09-22 23:33:55 +0700

    add user resource

---
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use Filament\Schemas\Schema;
+use Illuminate\Support\Facades\Hash;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Section;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Section::make('User Management')
+                    ->description('Manage users and their information.')
+                    ->schema([
+                        TextInput::make('name')
+                            ->required(),
+                        TextInput::make('email')
+                            ->label('Email address')
+                            ->unique(ignoreRecord: true)
+                            ->email()
+                            ->required(),
+                        TextInput::make('password')
+                            ->password()
+                            ->dehydrateStateUsing(fn(string $state): string => Hash::make($state))
+                            ->dehydrated(fn(?string $state): bool => filled($state))
+                            ->required(fn(string $operation): bool => $operation === 'create')
+                    ])
+                    ->columns(2),
+            ])
+            ->columns(1);
+    }
+}
